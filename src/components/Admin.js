57caa827afb5de71510d395a75476212033ed313
An Nguyen Truong <ntan@tma.com.vn>
2019-12-04 18:29:04 +0700

    Edit footer, update code upload image

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,22 +1,64 @@
 import React from 'react';
 
 class Admin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedFile: null,
+            category: '',
+            search_key: ''
+        }
+
+        this.onClickHandler = this.onClickHandler.bind(this);
+    }
+
+    updateCategory = (e) => {
+        this.setState({
+            category: e.target.value
+        });
+    }
+
+    updateSearchKey = (e) => {
+        this.setState({
+            search_key: e.target.value
+        });
+    }
+
+    onChangeHandler = e => {
+        this.setState({
+            selectedFile: e.target.files[0],
+            loaded: 0
+        })
+    }
+
+    onClickHandler = () => {
+        const data = new FormData();
+        data.append('file', this.state.selectedFile);
+        data.append('category', this.state.category);
+        data.append('search_key', this.state.search_key);
+        fetch("http://localhost:8080/api/upload/",
+            {
+                body: data,
+                method: "post"
+            }).then(res => alert('Upload successfully!')).catch(err => alert('Server Error!'));
+    }
+
     render() {
         return (
-            <form method='POST' action='http://localhost:́8080/api/upload'>
+            <form>
                 <div className='form-group'>
                     <label>Image file:</label>
-                    <input type='file' name='file' className='form-control' />
+                    <input type='file' name='file' className='form-control' onChange={this.onChangeHandler} />
                 </div>
                 <div className='form-group'>
                     <label>Category:</label>
-                    <input type='text' name='category' className='form-control' />
+                    <input type='text' name='category' className='form-control' value={this.state.inputValue} onChange={this.updateCategory} />
                 </div>
                 <div className='form-group'>
                     <label>Search key:</label>
-                    <input type='text' name='search_key' className='form-control' />
+                    <input type='text' name='search_key' className='form-control' value={this.state.inputValue} onChange={this.updateSearchKey} />
                 </div>
-                <button className='btn btn-primary' type='submit'>Upload</button>
+                <button className='btn btn-primary' type='button' onClick={this.onClickHandler}>Upload</button>
             </form>
         )
     }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer className='text-center'>
-                <span>Made by <font color='red'>2Red</font> -  NTA with ❤️️</span>
+                <span>Made by <font color='red'>2Red</font> aka  NTA with ❤️️</span>
             </footer>
         )
     }
