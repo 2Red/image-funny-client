b18f5c958ed89c71fdc3b8f2675743020506f0cb
An Nguyen <annguyen463@gmail.com>
2019-12-21 14:23:26 +0700

    Change icon, remove unuse file, chane font size Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return (
-            <footer className='text-center'>
+            <footer>
                 <span>Made by <font color='red'>2Red</font> aka  NTA with ❤️️</span>
             </footer>
         )
